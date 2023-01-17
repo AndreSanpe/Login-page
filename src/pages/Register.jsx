bf4e06b731f56ec1ca3cf1392e189633374c40c6
André Santos <x_sode@hotmail.com>
2023-01-17 14:07:12 -0300

    feat: register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import { Button, Link, TextField } from '@mui/material';
+import React from 'react';
+import { CustomForm } from '../styles/LoginForm';
+import { CustomPaper } from '../styles/LoginPaper';
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+function Register() {
 
-const Register = () => {
-    return (
-        <div>
-            <h1>Cadastrar</h1>
-            <div>
-                <label htmlFor="name"> Nome: 
-                <input type="text" id="name"/>
-                </label>
-                <label htmlFor="password"> Senha: 
-                <input type="text" id="password"/>
-                </label>
-                <button>Entrar</button>
-                <span>Registrar</span>
-            </div>
-        </div>
-    )
+
+  return (
+    <CustomForm >
+ 
+      <CustomPaper>
+      <h1>Faça seu Cadastro!</h1>
+      <TextField
+          id="outlined-required"
+          label="Name"
+        />      
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+      <Button variant="contained">Registrar</Button>
+      <Link href="/">
+
+<KeyboardReturnIcon />
+</Link>
+      </CustomPaper>
+    </CustomForm>
+  );
 }
 
 export default Register;
