95a6cec2782097ac08046f186859682769adf7c4
André Santos <x_sode@hotmail.com>
2023-01-17 14:09:15 -0300

    feat: create a new theme default

---
+++ src/styles/Theme.jsx
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#022C43",
+      },
+      backgroundColor: {
+        default: "#848484"
+      }
+    }
+  });
+
+  export default theme;
